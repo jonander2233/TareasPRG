d0df85d5466e5001bd234a5c5c72900e2c9b9aa6
Jon <jonander2233@gmail.com>
2023-11-22 17:38:59 +0100

    añadido ejercicio8

--- Tareast06/Seccion01/Ejercicio08.java
+++ Tareast06/Seccion01/Ejercicio08.java
@@ -2,14 +2,15 @@ package Tareast06.Seccion01;
 
 import libPersonal.EYS;
 
-public class Ejercicio07 {
+public class Ejercicio08 {
     public static void main (String [] args){
         String texto = EYS.imprimirYLeer("dime una frase");
-        texto=texto.trim();         //elimina los espacios al principio y al final
+        texto=texto.trim();
         String[] palabras = texto.split("\\s+"); // el ["\\s+"] toma uno o varios espacios por si el usuario coloca más de un espacio entre palabras
         for ( int i =0; i<palabras.length; i++){
-            EYS.imprimir(palabras[i]);
-
+            String palabra =palabras[i];
+            int tamano = palabra.length();
+            System.out.printf("%-15s %d%n", palabra, tamano);
         }
     }
     
