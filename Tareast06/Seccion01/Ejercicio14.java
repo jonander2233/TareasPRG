a2b3ce90efe3037576de68373a672b0fab6f6942
Jon <jonander2233@gmail.com>
2023-11-29 16:54:45 +0100

    terminado ejercicio 14

--- Tareast06/Seccion01/Ejercicio14.java
+++ Tareast06/Seccion01/Ejercicio14.java
@@ -1,11 +1,13 @@
 package Tareast06.Seccion01;
+import libPersonal.Arrays;
 import libPersonal.EYS;
 public class Ejercicio14 {
     public static void main (String[]args){
+        String resultado;
         char[] letras = new char[10];
-        EYS.leerArrayChar(letras);
-        for (int i=0;i>letras.length;i+=2){
-
-        }
+        EYS.imprimir("introduce 10 letras");
+        EYS.leerArrayChar(letras);              //pregunta al usuario el contenido del array y lo mete dentro de este
+        resultado = Arrays.arrayDevolverPares(letras);
+        EYS.imprimir(resultado);
     }
 }
--- libPersonal/Arrays.java
+++ libPersonal/Arrays.java
@@ -1,10 +1,20 @@
 package libPersonal;
 
 public class Arrays {
-    public static void arrayMostrarPares(char[]array){
-    for (int i=0;i>array.length;i+=2){
-        
+    public static String arrayDevolverPares(char[]letras){
+    StringBuilder resultado = new StringBuilder();          //llamada a stringBuilder y lo llama "resultado"
+
+    for (int i=1;i<letras.length;i+=2){
+        resultado.append(letras[i]);         //concatena los elementos del array
     }
-        
+    return resultado.toString();            //devuelve el conjunto de elemntos en forma de string 
+    }
+    public static String arrayDevolverImpares(char[]letras){
+    StringBuilder resultado = new StringBuilder();          //llamada a stringBuilder y lo llama "resultado"
+
+    for (int i=0;i<letras.length;i+=2){
+        resultado.append(letras[i]);         //concatena los elementos del array
+    }
+    return resultado.toString();            //devuelve el conjunto de elemntos en forma de string 
     }
 }
