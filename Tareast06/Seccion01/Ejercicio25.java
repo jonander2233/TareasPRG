2edebdd5927d00a00c28fc272a9e9313ac6695f5
Jon <jonander2233@gmail.com>
2023-12-15 15:30:22 +0100

    añadido ejercicio25

---
+++ Tareast06/Seccion01/Ejercicio25.java
@@ -0,0 +1,17 @@
+package tareast06.seccion01;
+import java.util.Random;
+
+public class Ejercicio25{
+    public static void Ejer25(){
+        Random aleatorio = new Random();
+        int tamanoArray = 50;
+        int[] miArray = new int[tamanoArray];
+
+        for (int i = 0; i < tamanoArray; i++) {
+            miArray[i] = aleatorio.nextInt(100); 
+        }
+
+    
+        
+    }
+}
