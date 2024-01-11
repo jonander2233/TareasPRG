a334ba3406f9559623dc863676a676488d72a7b7
Jon <jonander2233@gmail.com>
2024-01-11 19:54:41 +0100

    a

--- Tareast06/Seccion01/Ejercicio26.java
+++ Tareast06/Seccion01/Ejercicio26.java
@@ -3,7 +3,9 @@ import java.util.Random;
 import libpersonal.*;
 
 public class Ejercicio26 {
-    public static int[][] matriz = new int[4][8];
+    public static int FILAS=4;
+    public static int COLUMNAS=8;
+    public static int[][] matriz = new int[FILAS][COLUMNAS];
     public static void a(){
         Matrices.rellenarMatriz(matriz, 1, 9);
         for(int x = 0; x < matriz.length; x ++){
@@ -45,8 +47,30 @@ public class Ejercicio26 {
                     valMin=matriz[x][y];
                 }
             }
-
+            
         }
         Eys.imprimir("El valor maximo es: " + valMax + " Mientras que el valor minimo es :" + valMin);
+    }
+    public static void e(){
+        Matrices.rellenarMatriz(matriz, 1, 9);
+        int valor=0,contador=0;
+        double resultado;
+        for (int x=0;x<matriz.length;x++){
+            for (int y=0;y<matriz[x].length;y++){
+                contador++;
+                valor += matriz[x][y];
+            }
+        }
+        resultado = valor / contador;
+        Eys.imprimir("la media de todos los valores de la matriz es: " + resultado);
+    }
+    public static void f (){
+        Matrices.rellenarMatriz(matriz, 1, 9);
+        int fila = Eys.imprimirYLeerInt("Dime la fila?("+FILAS+")");
+        int columna = Eys.imprimirYLeerInt("Dime la columna?("+COLUMNAS+")");
+        int resultado = matriz[fila][columna];
+        Eys.imprimir("el numero almacenado en esa posicion es: "+ resultado);
+
+
     }
 }
